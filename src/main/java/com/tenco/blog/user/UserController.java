16b7f913d0c972d84ef602dc4e69e2bfeb0c6ab1
coramdeo643 <coramdeo643@kakao.com>
2025-07-22 10:14:15 +0900

    init

---
+++ src/main/java/com/tenco/blog/user/UserController.java
@@ -0,0 +1,128 @@
+package com.tenco.blog.user;
+
+import com.tenco.blog._core.errors.exception.Exception400;
+import com.tenco.blog.utils.Define;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor // DI 처리
+@Controller
+public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	private final UserService userService;
+	private final ProfileUploadService profileUploadService;
+
+	/**
+	 * 회원 정보 수정 화면 요청
+	 */
+	@GetMapping("/user/update-form")
+	public String updateForm(Model model, HttpSession session) {
+
+		// mustache 파일에서 sessionUser 키값 출력 하는 코드 있음
+		User sessionUser = (User) session.getAttribute("sessionUser");
+		User user = userService.findById(sessionUser.getId());
+		// 모델에서 관리하는 user 키값으로 mustache에
+		model.addAttribute("user", user);
+		return "user/update-form";
+	}
+
+	/**
+	 * 회원 수정 기능 요청
+	 */
+	@PostMapping("/user/update")
+	public String update(UserRequest.UpdateDTO reqDTO,
+						 HttpSession session) {
+		// 1. 인증검사
+		// 2. 유효성 검사
+		// 3. 서비스 계층 -> 회원 수정 기능 위임
+		// 4. 세션 동기화 처리
+		// 5. 리다이렉트 -> 회원 정보 화면 요청(새로운 request)
+		reqDTO.validate();
+		User user = (User) session.getAttribute("sessionUser");
+		User updateUser = userService.updateById(user.getId(), reqDTO);
+		session.setAttribute("sessionUser", updateUser);
+		return "redirect:/user/update-form";
+	}
+
+	@GetMapping("/join-form")
+	public String joinForm() {
+		log.info("회원 가입 요청 폼");
+		return "user/join-form";
+	}
+
+	/**
+	 * 회원 가입 기능 요청
+	 */
+	@PostMapping("/join")
+	public String join(UserRequest.JoinDTO joinDTO) {
+		joinDTO.validate();
+		userService.join(joinDTO);
+		return "redirect:/login-form";
+	}
+
+	/**
+	 * 로그인 화면 요청
+	 */
+	@GetMapping("/login-form")
+	public String loginForm() {
+		return "user/login-form";
+	}
+
+	/**
+	 * 로그인 요청
+	 */
+	@PostMapping("/login")
+	public String login(UserRequest.LoginDTO loginDTO, HttpSession session) {
+		loginDTO.validate();
+		User user = userService.login(loginDTO);
+		session.setAttribute(Define.SESSION_USER, user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
+
+	@PostMapping("/user/upload-profile-image")
+	public String uploadProfileImage(@RequestParam(name = "profileImage") MultipartFile multipartFile,
+									 HttpSession session) {
+
+		// 인증 검사는 인터 셉터에서 처리
+		User sessionUser = (User) session.getAttribute(Define.SESSION_USER);
+
+		// 유효성 검사 (파일 유효성 검사)
+		UserRequest.ProfileImageDTO profileImageDTO = new UserRequest.ProfileImageDTO();
+		profileImageDTO.setProfileImage(multipartFile);
+		profileImageDTO.validate();
+
+		// 서비스에게 일 위임( DB 저장 및 실제 파일 생성 까지)
+		User updateUser = userService.uploadProfileImage(sessionUser.getId(), multipartFile);
+
+		// 세션값에 새로운 값을 재 갱신 해주어야 한다. (세션 재 갱신 처리)
+		session.setAttribute(Define.SESSION_USER, updateUser);
+
+		// 업로드 로직 구현 시작 ....
+		return "redirect:/user/update-form";
+	}
+
+	@PostMapping("/user/delete-profile-image")
+	public String deleteProfileImage(HttpSession session) {
+		User sessionUser = (User) session.getAttribute(Define.SESSION_USER);
+		// DB 경로를 null 처리하고 실제 파일도 삭제처리한다
+		User updateUser = userService.deleteProfileImage(sessionUser.getId());
+		// 세선 정보 업데이트 처리
+		session.setAttribute(Define.SESSION_USER, updateUser);
+
+		return "redirect:/user/update-form";
+	}
+}
---
+++ src/main/java/com/tenco/blog/user/UserService.java
@@ -0,0 +1,117 @@
+package com.tenco.blog.user;
+
+import com.tenco.blog._core.errors.exception.Exception400;
+import com.tenco.blog._core.errors.exception.Exception404;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true) // 클래스 레벨에서의 읽기 전용 설정
+public class UserService {
+
+	private static final Logger log = LoggerFactory.getLogger(UserService.class);
+	private final UserJpaRepository userJpaRepository;
+	private final ProfileUploadService profileUploadService;
+
+	/**
+	 * 프로필 이미지 업로드 서비스(DB 저장까지)
+	 *
+	 * @param userId
+	 * @param multipartFile
+	 * @return
+	 */
+	@Transactional
+	public User uploadProfileImage(Long userId, MultipartFile multipartFile) {
+
+		User user = findById(userId);
+		// 최초등록, 수정시도 있음
+		String oldImagePath = user.getProfileImagePath();
+
+		try {
+			// 1. 새 이미지를 서버 컴퓨터에 생성 완료...
+			String newImagePath = profileUploadService.uploadProfileImage(multipartFile);
+			// 2. 기존 이미지가 있다면 서버 컴퓨터에서 파일 제거(공간 부족해지니까)
+			if (oldImagePath != null) {
+				profileUploadService.deleteProfileImage(oldImagePath);
+			}
+			// 3. db에 저장 Dirty Checking 활용
+			user.setProfileImagePath(newImagePath);
+			// TODO -
+			return user;
+		} catch (IOException e) {
+			throw new Exception400("프로필 이미지 업로드에 실패했습니다");
+		}
+	}
+
+	/**
+	 * 회원가입 처리
+	 */
+	@Transactional // 메서드 레벨에서 쓰기 전용 트랜잭션 활성화
+	public User join(UserRequest.JoinDTO joinDTO) {
+		//1. 사용자명 중복 체크
+		userJpaRepository.findByUsername(joinDTO.getUsername())
+				.ifPresent(user1 -> {
+					throw new Exception400("이미 존재하는 사용자명입니다");
+				});
+		return userJpaRepository.save(joinDTO.toEntity());
+	}
+
+	/**
+	 * 로그인 처리
+	 */
+	public User login(UserRequest.LoginDTO loginDTO) {
+		return userJpaRepository
+				.findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword())
+				.orElseThrow(() -> {
+					return new Exception400("사용자명 또는 비밀번호가 틀렸어요");
+				});
+	}
+
+	/**
+	 * 사용자 정보 조회
+	 */
+	public User findById(Long id) {
+		return userJpaRepository.findById(id).orElseThrow(() -> {
+			log.warn("사용자 조회 실패 - ID {}", id);
+			return new Exception404("사용자를 찾을 수 없습니다");
+		});
+	}
+
+	/**
+	 * 회원정보 수정 처리 (더티 체킹)
+	 */
+	@Transactional
+	public User updateById(Long userId, UserRequest.UpdateDTO updateDTO) {
+		// 1.
+		// 2. 사용자 조회
+		// 3. 수정된 User 반환 왜? --> 세션 동기화 때문!!!
+		User user = findById(userId);
+		// user.update(updateDTO);  TODO 추후 추가
+		user.setPassword(updateDTO.getPassword());
+		return user;
+	}
+
+	// 이미지 삭제만 처리하는 메서드
+	@Transactional
+	public User deleteProfileImage(Long userId) {
+		User user = findById(userId);
+		// DB 저장된 이미지 경로 추출
+		String imagePath = user.getProfileImagePath();
+		// 이미지 삭제 로직
+		user.setProfileImagePath(null); // 엔티티 상태값 변경 -> 자동 수정
+		if (imagePath != null && imagePath.isEmpty() == false) {
+			// 실제 서버에 존재하는 파일을 삭제 처리
+			profileUploadService.deleteProfileImage(imagePath);
+		}
+		// 변경된 엔티티를 리턴
+		return user;
+	}
+
+}
